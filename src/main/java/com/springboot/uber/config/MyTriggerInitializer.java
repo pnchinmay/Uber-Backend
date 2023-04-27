1e9d866721021603db9563c0be6d266dcfeaa9d5
pnchinmay <pnchinmay9@gmail.com>
2023-04-27 17:15:47 +0530

    chore(useless): add triggers & other SQL queries for showcase

--- src/main/java/com/springboot/uber/config/MyTriggerInitializer.java
+++ src/main/java/com/springboot/uber/config/MyTriggerInitializer.java
@@ -22,5 +22,15 @@ public class MyTriggerInitializer {
 				+ "create trigger update_vehicle_id after update on driver for each row begin update ride set vehicle_id=NEW.vehicle_id WHERE driver_id =NEW.id; END"
 				+ "$$" + "delimiter ;");
 
+		// Non-conflict Serializable SQL Transactions - check BookingController
+		jdbcTemplate.execute("START TRANSACTION;\n"
+				+ "insert into vehicle values(590,'1G6DL67A890323058',1,'Maroon','Volkswagen','Passat','petrol','sedan',1);\n"
+				+ "COMMIT;");
+
+		// Non-conflict Serializable SQL Transactions - check BookingController
+		jdbcTemplate.execute("START TRANSACTION;\n"
+				+ "insert into driver values(520,'1825-59-4713','Stockton','Trailsway','95210','California','407',47,'01GRVQ8A61K5BN2GAAV2BDTH20','hroycroft0@dmoz.org',4,'Harriot','F','Roycroft','Harriot@95210Roycroft','209-359-9110',1,590);\n"
+				+ "Delete from vehicle where id = 590;\n" + "COMMIT;");
+
 	}
 }
