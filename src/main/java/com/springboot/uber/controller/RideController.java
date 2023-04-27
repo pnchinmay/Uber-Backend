09f47a996515eda402fd8ed332d363e22b6478b1
pnchinmay <pnchinmay9@gmail.com>
2023-04-27 17:04:15 +0530

    refactor: add custom PUT mapping for rides using @Query annotation and not uisng JPA methods
    - Find how to update without this custom method & avoiding the below error which gets thrown during @PutMapping using JPA methods
    - need to figure out how to resolve the error
    - Error: "Multiple representations of the same entity [com.springboot.uber.model.Vehicle#614] are being merged. Detached: [com.springboot.uber.model.Vehicle@1ab7ed95]; Detached: [com.springboot.uber.model.Vehicle@4c3b48d2]; nested exception is java.lang.IllegalStateException: Multiple representations of the same entity [com.springboot.uber.model.Vehicle#614] are being merged. Detached: [com.springboot.uber.model.Vehicle@1ab7ed95]; Detached: [com.springboot.uber.model.Vehicle@4c3b48d2]"

--- src/main/java/com/springboot/uber/controller/RideController.java
+++ src/main/java/com/springboot/uber/controller/RideController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.springboot.uber.repository.RideRepository;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "*")
 public class RideController {
 
 	@Autowired
@@ -36,7 +38,7 @@ public class RideController {
 		return rideRepository.save(ride);
 	}
 
-	@GetMapping("/ride/{id}")
+	@GetMapping("/rides/{id}")
 	public ResponseEntity<Ride> getRidebyId(@PathVariable Long id) {
 		Ride ride = rideRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No ride of id: " + id));
@@ -45,21 +47,41 @@ public class RideController {
 
 	@PutMapping("/rides/{id}")
 	public ResponseEntity<Ride> updateRidebyId(@PathVariable Long id, @RequestBody Ride rideDetails) {
-		Ride ride = rideRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("No ride of id: " + id));
-		ride.setPickup(rideDetails.getPickup());
-		ride.setDestination(rideDetails.getDestination());
-		ride.setDuration(rideDetails.getDuration());
-		ride.setFare(rideDetails.getFare());
-		ride.setStatus(rideDetails.getStatus());
-		ride.setDistance(rideDetails.getDistance());
-		ride.setVehicle(rideDetails.getVehicle());
-		ride.setDriver(rideDetails.getDriver());
+		String pickup = rideDetails.getPickup();
+		String destination = rideDetails.getDestination();
+		int duration = rideDetails.getDuration();
+		int fare = rideDetails.getFare();
+		int status = rideDetails.getStatus();
+		int distance = rideDetails.getDistance();
+		Long driver_id = rideDetails.getDriver().getId();
+		Long vehicle_id = rideDetails.getVehicle().getId();
 
-		Ride updatedRide = rideRepository.save(ride);
-		return ResponseEntity.ok(updatedRide);
+		rideRepository.update(id, pickup, destination, duration, fare, status, distance, driver_id, vehicle_id);
+
+//		Find how to update without this custom method & avoiding the below error which gets thrown during normal @PutMapping described below:
+//	"Multiple representations of the same entity [com.springboot.uber.model.Vehicle#614] are being merged. Detached: [com.springboot.uber.model.Vehicle@1ab7ed95]; Detached: [com.springboot.uber.model.Vehicle@4c3b48d2]; nested exception is java.lang.IllegalStateException: Multiple representations of the same entity [com.springboot.uber.model.Vehicle#614] are being merged. Detached: [com.springboot.uber.model.Vehicle@1ab7ed95]; Detached: [com.springboot.uber.model.Vehicle@4c3b48d2]"
+
+		Ride ride = rideRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No payment of id: " + id));
+		return ResponseEntity.ok(ride);
 	}
 
+	/*
+	 * @PutMapping("/rides/{id}") public ResponseEntity<Ride>
+	 * updateRidebyId(@PathVariable Long id, @RequestBody Ride rideDetails) { Ride
+	 * ride = rideRepository.findById(id) .orElseThrow(() -> new
+	 * ResourceNotFoundException("No ride of id: " + id));
+	 * ride.setPickup(rideDetails.getPickup());
+	 * ride.setDestination(rideDetails.getDestination());
+	 * ride.setDuration(rideDetails.getDuration());
+	 * ride.setFare(rideDetails.getFare()); ride.setStatus(rideDetails.getStatus());
+	 * ride.setDistance(rideDetails.getDistance());
+	 * ride.setVehicle(rideDetails.getVehicle());
+	 * ride.setDriver(rideDetails.getDriver());
+	 * 
+	 * Ride updatedRide = rideRepository.save(ride); return
+	 * ResponseEntity.ok(updatedRide); }
+	 */
 	@DeleteMapping("/rides/{id}")
 	public ResponseEntity<Map<String, Boolean>> deleteRide(@PathVariable Long id) {
 		Ride ride = rideRepository.findById(id)
